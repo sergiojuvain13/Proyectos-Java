4cf5b8106e80bd60eded327972a00037978665d6
sergiojuvain13 <sergiojuvain@gmail.com>
2026-04-07 16:16:26 +0200

    Empezando con WindowsBuilder

---
+++ Nova DAM/src/ejercicios/Visualizacion1.java
@@ -0,0 +1,79 @@
+package ejercicios;
+
+import java.awt.EventQueue;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Visualizacion1 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Visualizacion1 frame = new Visualizacion1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Visualizacion1() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 436, 21);
+		contentPane.add(menuBar);
+
+		JMenu mnNewMenu = new JMenu("Cambiar color");
+		menuBar.add(mnNewMenu);
+
+		JMenuItem mntmNewMenuItem = new JMenuItem("rojo");
+		mntmNewMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				contentPane.setBackground(Color.red);
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem);
+
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("verde");
+		mntmNewMenuItem_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				contentPane.setBackground(Color.GREEN);
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem_1);
+
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("azul");
+		mntmNewMenuItem_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				contentPane.setBackground(Color.BLUE);
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem_2);
+
+	}
+
+}
